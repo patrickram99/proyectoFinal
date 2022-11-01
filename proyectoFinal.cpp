22529eeb5f7ec71c82fde932b8763f868f02a44f
patrickram99 <pramirezs@ulasalle.edu.pe>
2022-11-01 11:02:03 -0500

    Mantenimiento (mostrar y listar telas)
    Se puso un vector dinamico en los tipos de telas para almacenar las ventas en el futuro, ademas el archivo se lee desde una funcion

--- proyectoFinal.cpp
+++ proyectoFinal.cpp
@@ -1,52 +1,40 @@
-#include <fstream>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "tela.h"
 
 int main()
 {
-    const int numTelas = 5;
-    tela datosTelas[numTelas];
-    std::fstream telasData;
-    telasData.open("telas-data.txt", std::ios::in);
-    // Leer y almacenar los datos del archivo en un array
-    if(telasData.is_open()){
-        string linea;
-        int j = 0;
-        int j1 = 0;
-        int j2 = 0;
-        int j3 = 0;
-        int j4 = 0;
-        int j5 = 0;
-        for(int i = 1; i <= 30; ++i){
-            std::getline(telasData, linea);
-            if((i - 1) % 6 == 0){
-                datosTelas[j].identificador = linea;
-                j++;
-            }
-            if((i - 2) % 6 == 0){
-                datosTelas[j1].nombre = linea;
-                j1++;
-            }
-            if((i - 3) % 6 == 0){
-                datosTelas[j2].composicion = linea;
-                j2++;
-            }
-            if((i - 4) % 6 == 0){
-                datosTelas[j3].ancho = std::stoi(linea);
-                j3++;
-            }
-            if((i - 5) % 6 == 0){
-                datosTelas[j4].peso = std::stoi(linea);
-                j4++;
-            }
-            if((i - 6) % 6 == 0){
-                datosTelas[j5].precio = std::stod(linea);
-                j5++;
+    //Declaramos un vector dinamico para almacenar los datos de las telas
+    int numTelas = 5;
+    tela* datosTelas;
+    datosTelas = new tela[numTelas];
+
+    almacenarDatos(datosTelas,"telas-data.txt");
+
+    string opcion;
+    std::cout << "Elija una opcion: Mantenimiento, Vender, Reportes, Configuracion o Salir"<< std::endl;
+    std::getline(std::cin, opcion);
+
+    if(opcion == "Mantenimiento"){
+        std::cout <<"Desea Consultar tela, Modificar tela o Listar"<<std::endl;
+        std::getline(std::cin, opcion);
+        if(opcion == "Consultar tela"){
+            std::cout << "Ingrese el identificador de la tela a consultar: " << std::endl;
+            std::cin >> opcion;
+            std::cout << datosTelas[std::stoi(opcion.substr(2, 1))];
+        } else if(opcion == "Modificar tela"){
+            std::cout << "Ingrese el identificador de la tela a modificar: " << std::endl;
+            std::cin >> opcion;
+        } else{
+            std::cout << "LISTADO DE TELAS: \n" << std::endl;
+            for(int i = 0; i < 5; ++i){
+                std::cout << datosTelas[i] << std::endl << std::endl;
             }
         }
     }
 
-    
+
+    delete[] datosTelas;
     return 0;
 }
--- tela.h
+++ tela.h
@@ -1,5 +1,6 @@
 #ifndef PROYECTOFINAL_TELA_H
 #define PROYECTOFINAL_TELA_H
+#include <fstream>
 using std::string;
 
 class tela{
@@ -10,7 +11,56 @@ public:
     int ancho;
     int peso;
     double precio;
+
+    friend std::ostream& operator << (std::ostream& os, const tela& p)
+    {
+        return os << "Codigo "
+                  << p.identificador << ": " << std::endl
+                  << "Descripcion: "<< p.nombre << "\n"
+                  << "Material: "<< p.composicion << "\n"
+                  << "Ancho: "<< p.ancho << "\n"
+                  << "Peso: "<< p.peso << "\n"
+                  << "Precio: "<< p.precio;
+    }
 };
+
+void almacenarDatos(tela* datosTelas, string fichero){
+    std::fstream telasFichero;
+    telasFichero.open(fichero, std::ios::in);
+    // Leer y almacenar los datos del archivo en un array
+    if(telasFichero.is_open()){
+        string linea;
+        int j = 0;
+        int j1 = 0;
+        int j2 = 0;
+        int j3 = 0;
+        int j4 = 0;
+        int j5 = 0;
+        for(int i = 1; i <= 30; ++i){
+            std::getline(telasFichero, linea);
+            if((i - 1) % 6 == 0){
+                datosTelas[j].identificador = linea;
+                j++;
+            } else if((i - 2) % 6 == 0){
+                datosTelas[j1].nombre = linea;
+                j1++;
+            } else if((i - 3) % 6 == 0){
+                datosTelas[j2].composicion = linea;
+                j2++;
+            } else if((i - 4) % 6 == 0){
+                datosTelas[j3].ancho = std::stoi(linea);
+                j3++;
+            } else if((i - 5) % 6 == 0){
+                datosTelas[j4].peso = std::stoi(linea);
+                j4++;
+            } else{
+                datosTelas[j5].precio = std::stod(linea);
+                j5++;
+            }
+        }
+    }
+    telasFichero.close();
+}
 
 
 #endif //PROYECTOFINAL_TELA_H
