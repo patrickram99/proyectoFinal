d7facbc06b9655f1c54178eb7d222fbc6536cc6b
patrickram99 <pramirezs@ulasalle.edu.pe>
2022-11-06 13:43:49 -0500

    Optimizacion funciones
    Se cambio a funcion almacenarDatos para que soolo use un for, se creo funciones de consultarTela, modificarTela y listarTela. Por ultimo se creo la funcion imprimir en la clase tela para mostrar con mayor facilidad en listar y consuktar tela

--- proyectoFinal.cpp
+++ proyectoFinal.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 #include "tela.h"
 
 void prueba(){
@@ -12,30 +11,29 @@ int main()
     tela* datosTelas;
     datosTelas = new tela[numTelas];
 
+    //Almacenamos los datos del ficero en el array datosTelas
     almacenarDatos(datosTelas,"telas-data.txt");
 
+    //Comenzamos con el menú de opciones
     string opcion;
     std::cout << "Elija una opcion: Mantenimiento, Vender, Reportes, Configuracion o Salir"<< std::endl;
     std::getline(std::cin, opcion);
 
     if(opcion == "Mantenimiento"){
-        std::cout <<"Desea Consultar tela, Modificar tela o Listar"<<std::endl;
+        std::cout << "Desea Consultar tela, Modificar tela o Listar" << std::endl;
         std::getline(std::cin, opcion);
         if(opcion == "Consultar tela"){
-            std::cout << "Ingrese el identificador de la tela a consultar: " << std::endl;
-            std::cin >> opcion;
-            std::cout << datosTelas[std::stoi(opcion.substr(2, 1))];
+            consultarTela(datosTelas);
         } else if(opcion == "Modificar tela"){
-            std::cout << "Ingrese el identificador de la tela a modificar: " << std::endl;
-            std::cin >> opcion;
+            modificarTela(datosTelas);
         } else{
-            std::cout << "LISTADO DE TELAS: \n" << std::endl;
-            for(int i = 0; i < 5; ++i){
-                std::cout << datosTelas[i] << std::endl << std::endl;
-            }
+            listarTelas(datosTelas);
         }
-    }
+    } else if(opcion == "Vender"){
 
+    } else{
+
+    }
 
     delete[] datosTelas;
     return 0;
--- tela.h
+++ tela.h
@@ -1,66 +1,86 @@
 #ifndef PROYECTOFINAL_TELA_H
 #define PROYECTOFINAL_TELA_H
 #include <fstream>
+#include <vector>
+
 using std::string;
 
 class tela{
 public:
-    string identificador;
-    string nombre;
-    string composicion;
-    int ancho;
-    int peso;
-    double precio;
+    string              identificador;
+    string              nombre;
+    string              composicion;
+    int                 ancho;
+    int                 peso;
+    double              precio;
 
-    friend std::ostream& operator << (std::ostream& os, const tela& p)
-    {
-        return os << "Codigo "
-                  << p.identificador << ": " << std::endl
-                  << "Descripcion: "<< p.nombre << "\n"
-                  << "Material: "<< p.composicion << "\n"
-                  << "Ancho: "<< p.ancho << "\n"
-                  << "Peso: "<< p.peso << "\n"
-                  << "Precio: "<< p.precio;
+    void imprimir() const{
+        std::cout << "Codigo "      << identificador << ": \n"
+                  << "Descripcion: "<< nombre        << "\n"
+                  << "Material: "   << composicion   << "\n"
+                  << "Ancho: "      << ancho         << "\n"
+                  << "Peso: "       << peso          << "\n"
+                  << "Precio: "     << precio        << "\n";
+
     }
 };
 
-void almacenarDatos(tela* datosTelas, string fichero){
+class ventas{
+public:
+    int identificador;
+
+};
+void almacenarDatos(tela *datosTelas, string fichero) {
+    string linea;
     std::fstream telasFichero;
     telasFichero.open(fichero, std::ios::in);
     // Leer y almacenar los datos del archivo en un array
-    if(telasFichero.is_open()){
-        string linea;
-        int j = 0;
-        int j1 = 0;
-        int j2 = 0;
-        int j3 = 0;
-        int j4 = 0;
-        int j5 = 0;
-        for(int i = 1; i <= 30; ++i){
+        for (size_t i = 0; i < 5; ++i){
+            std::getline(telasFichero, datosTelas[i].identificador);
+            std::getline(telasFichero, datosTelas[i].nombre);
+            std::getline(telasFichero, datosTelas[i].composicion);
             std::getline(telasFichero, linea);
-            if((i - 1) % 6 == 0){
-                datosTelas[j].identificador = linea;
-                j++;
-            } else if((i - 2) % 6 == 0){
-                datosTelas[j1].nombre = linea;
-                j1++;
-            } else if((i - 3) % 6 == 0){
-                datosTelas[j2].composicion = linea;
-                j2++;
-            } else if((i - 4) % 6 == 0){
-                datosTelas[j3].ancho = std::stoi(linea);
-                j3++;
-            } else if((i - 5) % 6 == 0){
-                datosTelas[j4].peso = std::stoi(linea);
-                j4++;
-            } else{
-                datosTelas[j5].precio = std::stod(linea);
-                j5++;
-            }
+            datosTelas[i].ancho = std::stoi(linea);
+            std::getline(telasFichero, linea);
+            datosTelas[i].peso = std::stoi(linea);
+            std::getline(telasFichero, linea);
+            datosTelas[i].precio = std::stod(linea);
         }
-    }
-    telasFichero.close();
+        telasFichero.close();
 }
 
+void consultarTela(const tela* datosTelas){
+    string opcion;
+    std::cout << "Ingrese el identificador de la tela a consultar: " << std::endl;
+    std::cin >> opcion;
+    datosTelas[std::stoi(opcion.substr(2, 1))].imprimir();
+}
+
+void modificarTela(tela* &datosTelas) {
+    string opcion;
+    std::vector<string> aspectos = {"Identificador", "Descripcion", "Material", "Ancho", "Peso", "Precio"};
+    std::cout << "Ingrese el identificador de la tela a modificar: " << std::endl;
+    std::cin >> opcion;
+    for(int i = 0; i < 5; ++i){
+        if(std::stoi(opcion.substr(2, 1)) == i){
+            std::cout << "Que aspecto desea modificar: ";
+            std::cin >> opcion;
+            for(size_t j = 0; j < aspectos.size(); ++j){
+                if(opcion == aspectos[j]){
+
+                }
+            }
+
+        }
+    }
+}
+
+void listarTelas(const tela* datosTelas){
+    std::cout << "LISTADO DE TELAS: \n" << std::endl;
+    for(int i = 0; i < 5; ++i){
+        datosTelas[i].imprimir();
+        std::cout << std::endl;
+    }
+}
 
 #endif //PROYECTOFINAL_TELA_H
